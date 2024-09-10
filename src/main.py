fb687ccdaaab906c91d0937b60251ea2eaf57c9e
OsmaiR_RiamsO <85657931+Osmair-riamso@users.noreply.github.com>
2024-09-10 20:43:20 -0300

    atualizando Main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+from random import randint
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
+
+class Estudante(BaseModel):
+    nome:str
+    curso:str
+    ativo:bool
 
 #http://127.0.0.1:8000
 @app.get("/")
@@ -8,7 +15,9 @@ async def root():
     return {"message": "Olá Mundo"}
 
 #http://127.0.0.1:8000/teste1
-@app.get('/teste1')
+@app.get('/funcaoteste')
 async def funcaoteste():
-    return {'Teste deu certo!!!'}
+    return {"Teste": True, "num_alatorio": random.randint(a: 0, b: 20000)}
+
+
 
